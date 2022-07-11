9a59cccf6b2866014ff93dc84cfd6a1e322583e6
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-11 17:48:06 +0200

    added nav of google page

---
+++ index.php
@@ -0,0 +1,85 @@
+<!--
+Riscrivere questa pagina del sito google
+https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP.
+-->
+
+
+
+
+<?php 
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+   
+    <script src="https://kit.fontawesome.com/34ba7cedd4.js" crossorigin="anonymous"></script>
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid border-bottom">
+        <!-- container up -->
+        <div class="container-fluid d-flex justify-content-between">
+            <!-- container di sinistra -->
+            <div>
+                <span>GOOGLE</span>
+                <span> Privacy e Termini</span>
+            </div>
+            <!-- container di destra -->
+            <div>
+                <span><i class="fa-solid fa-grip-dots"></i></span>
+                <span>
+                    <img src="" alt="">
+                </span>
+            </div>
+        </div>
+
+        <!-- container down -->
+        <div class="container-fluid d-flex my-3">
+            <div class="me-2">
+                <a>Introduzione</a>
+            </div>
+
+            <div class="mx-2">
+                <a>Norme sulla privacy</a>
+            </div>
+
+            <div class="mx-2">
+                <a>Termini di servizio</a>
+            </div>
+
+            <div class="mx-2">
+                <a>Tecnologie</a>
+            </div>
+
+            <div class="mx-2">
+                <a href="#">Domande Frequenti</a>
+            </div>
+        </div>
+
+    </div>
+
+
+        <!-- container with title and paragraph -->
+        <div>
+
+        </div>
+        
+    
+</body>
+</html>
